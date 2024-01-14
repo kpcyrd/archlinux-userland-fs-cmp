c4383d54d2ea0923f0cdaa5cbcf9dd9407c95078
kpcyrd <git@rxv.cc>
2024-01-14 01:58:06 +0100

    Refactor rendering code and put state in a struct

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,6 @@ use colored::{Color, Colorize};
 use env_logger::Env;
 use num_format::{Locale, ToFormattedString};
 use std::collections::{HashMap, HashSet, VecDeque};
-use std::fmt::Write;
 use std::path::PathBuf;
 use tokio::sync::mpsc;
 use tokio::sync::oneshot;
@@ -34,6 +33,210 @@ pub enum Event {
     CompletedHashing(HashVerify),
 }
 
+#[derive(Default)]
+pub struct App {
+    num_hash_worker: usize,
+    retired_hashers: usize,
+
+    completed_pkgs: u64,
+    total_pkgs: u64,
+    trusted_hashes: HashMap<PathBuf, String>,
+
+    running_list_installed: bool,
+    running_disk_scan: bool,
+
+    waiting_for_data: HashSet<PathBuf>,
+    waiting_for_hasher: VecDeque<(PathBuf, String)>,
+    available_hashers: VecDeque<oneshot::Sender<(PathBuf, String)>>,
+
+    files_passed: u64,
+    files_flagged: u64,
+
+    disk_pwd: Option<PathBuf>,
+}
+
+impl App {
+    fn new(num_hash_worker: usize) -> Self {
+        Self {
+            num_hash_worker,
+            running_list_installed: true,
+            running_disk_scan: true,
+            ..Default::default()
+        }
+    }
+
+    fn update(&mut self, event: Event) -> bool {
+        match event {
+            Event::PkgQueued => self.total_pkgs += 1,
+            Event::PkgCompleted => {
+                self.completed_pkgs += 1;
+                return true;
+            }
+            Event::TrustedFile(path, sha256) => {
+                if let Some(old) = self.trusted_hashes.get(&path) {
+                    warn!("Unexpected duplicate for {path:?} ({sha256:?} vs {old:?})");
+                } else {
+                    if self.waiting_for_data.remove(&path) {
+                        self.waiting_for_hasher
+                            .push_back((path.clone(), sha256.clone()));
+                    }
+                    self.trusted_hashes.insert(path, sha256);
+                }
+            }
+            Event::DiskFile(path) => {
+                if let Some(sha256) = self.trusted_hashes.get(&path) {
+                    self.waiting_for_hasher.push_back((path, sha256.clone()));
+                } else {
+                    self.waiting_for_data.insert(path);
+                }
+            }
+            Event::DiskPwd(path) => {
+                self.disk_pwd = Some(path);
+            }
+            Event::CompletedListInstalled => {
+                self.running_list_installed = false;
+                return true;
+            }
+            Event::CompletedDiskScan => {
+                self.running_disk_scan = false;
+                self.disk_pwd = None;
+                return true;
+            }
+            Event::AvailableHasher(hasher) => {
+                self.available_hashers.push_back(hasher);
+            }
+            Event::CompletedHashing(hashed) => match hashed {
+                HashVerify::Passed(_) => self.files_passed += 1,
+                HashVerify::Flagged(_) => self.files_flagged += 1,
+            },
+        }
+
+        false
+    }
+
+    fn redraw(&self, logs_enabled: bool) {
+        let mut status = "packages: ".bold().to_string();
+        status.push_str(
+            &format!(
+                "{:>7}",
+                self.completed_pkgs.to_formatted_string(&Locale::en)
+            )
+            .color(if self.completed_pkgs < self.total_pkgs {
+                Color::Yellow
+            } else {
+                Color::Green
+            })
+            .to_string(),
+        );
+        status.push_str(&"/".bold().to_string());
+        status.push_str(
+            &self
+                .total_pkgs
+                .to_formatted_string(&Locale::en)
+                .bold()
+                .to_string(),
+        );
+        if self.running_list_installed {
+            status.push_str("...");
+        }
+
+        if !self.trusted_hashes.is_empty() {
+            status.push_str(
+                &format!(
+                    " (files: {:>7})",
+                    self.trusted_hashes.len().to_formatted_string(&Locale::en)
+                )
+                .bright_black()
+                .to_string(),
+            );
+        }
+
+        status.push_str(&" | scanned disk: ".bold().to_string());
+        status.push_str(
+            &format!(
+                "{:>8}",
+                self.waiting_for_data.len().to_formatted_string(&Locale::en)
+            )
+            .yellow()
+            .to_string(),
+        );
+        if self.running_disk_scan {
+            status.push_str("...");
+        }
+        status.push('/');
+        status.push_str(&format!(
+            "{:>7}",
+            self.waiting_for_hasher
+                .len()
+                .to_formatted_string(&Locale::en)
+        ));
+
+        status.push_str(&" | hashing ".bold().to_string());
+        {
+            let running_hash_workers =
+                self.num_hash_worker - self.retired_hashers - self.available_hashers.len();
+            let s = format!("{running_hash_workers}/{}", self.num_hash_worker);
+            let s = if running_hash_workers == self.num_hash_worker {
+                s.cyan()
+            } else if running_hash_workers == 0 {
+                s.bright_black()
+            } else {
+                s.normal()
+            }
+            .to_string();
+            status.push('[');
+            status.push_str(&s);
+            status.push(']');
+        }
+
+        status.push_str(&" | passed".bold().to_string());
+        status.push('=');
+        status.push_str(
+            &self
+                .files_passed
+                .to_formatted_string(&Locale::en)
+                .green()
+                .to_string(),
+        );
+        status.push_str(&" failed".bold().to_string());
+        status.push('=');
+        status.push_str(
+            &self
+                .files_flagged
+                .to_formatted_string(&Locale::en)
+                .red()
+                .to_string(),
+        );
+
+        if let Some(path) = &self.disk_pwd {
+            let path = format!("{:?}", path.display());
+
+            let mut offset = 0;
+            let mut width = 0;
+            let mut truncated = false;
+            for c in path.chars() {
+                width += unicode_width::UnicodeWidthChar::width(c).unwrap_or(1);
+                if width < PATH_TRUNCATE {
+                    offset += c.len_utf8();
+                } else {
+                    truncated = true;
+                    break;
+                }
+            }
+
+            let path = &path[..offset];
+            let truncated = if truncated { "..." } else { "" };
+            status.push_str(&format!(" ({path}{truncated})").bright_black().to_string());
+        }
+
+        if logs_enabled {
+            info!("{status}");
+        } else {
+            println!("{status}");
+        }
+    }
+}
+
 #[tokio::main]
 async fn run(args: Args) -> Result<()> {
     let dbpath = args.path.join(&args.dbpath);
@@ -47,20 +250,7 @@ async fn run(args: Args) -> Result<()> {
     let num_hash_worker = args.concurrency.unwrap_or_else(num_cpus::get);
     disk::spawn_scan(event_tx, args.path, excluded, num_hash_worker);
 
-    let mut completed_pkgs = 0;
-    let mut total_pkgs = 0;
-    let mut trusted_hashes = HashMap::new();
-
-    let mut running_list_installed = true;
-    let mut running_disk_scan = true;
-
-    let mut waiting_for_data = HashSet::new();
-    let mut waiting_for_hasher = VecDeque::new();
-    let mut available_hashers = VecDeque::new();
-    let mut files_passed = 0;
-    let mut files_flagged = 0;
-
-    let mut disk_pwd = None;
+    let mut app = App::new(num_hash_worker);
 
     let mut interval = time::interval(if args.verbose == 0 {
         Duration::from_millis(500)
@@ -72,50 +262,13 @@ async fn run(args: Args) -> Result<()> {
     loop {
         tokio::select! {
             event = event_rx.recv() => {
-                match event {
-                    Some(Event::PkgQueued) => total_pkgs += 1,
-                    Some(Event::PkgCompleted) => {
-                        completed_pkgs += 1;
+                if let Some(event) = event {
+                    if app.update(event) {
                         redraw = true;
                     }
-                    Some(Event::TrustedFile(path, sha256)) => {
-                        if let Some(old) = trusted_hashes.get(&path) {
-                            warn!("Unexpected duplicate for {path:?} ({sha256:?} vs {old:?})");
-                        } else {
-                            if waiting_for_data.remove(&path) {
-                                waiting_for_hasher.push_back((path.clone(), sha256.clone()));
-                            }
-                            trusted_hashes.insert(path, sha256);
-                        }
-                    }
-                    Some(Event::DiskFile(path)) => {
-                        if let Some(sha256) = trusted_hashes.get(&path) {
-                            waiting_for_hasher.push_back((path, sha256.clone()));
-                        } else {
-                            waiting_for_data.insert(path);
-                        }
-                    }
-                    Some(Event::DiskPwd(path)) => {
-                        disk_pwd = Some(path);
-                    }
-                    Some(Event::CompletedListInstalled) => {
-                        running_list_installed = false;
-                        redraw = true;
-                    }
-                    Some(Event::CompletedDiskScan) => {
-                        running_disk_scan = false;
-                        disk_pwd = None;
-                        redraw = true;
-                    }
-                    Some(Event::AvailableHasher(hasher)) => {
-                        available_hashers.push_back(hasher);
-                    }
-                    Some(Event::CompletedHashing(hashed)) => match hashed {
-                        HashVerify::Passed(_) => files_passed += 1,
-                        HashVerify::Flagged(_) => files_flagged += 1,
-                    }
+                } else {
                     // everything has shutdown
-                    None => break,
+                    break
                 }
             }
             _ = interval.tick() => {
@@ -123,135 +276,32 @@ async fn run(args: Args) -> Result<()> {
             }
         }
 
-        while !waiting_for_hasher.is_empty() && !available_hashers.is_empty() {
-            let hasher = available_hashers.pop_front().unwrap();
-            let task = waiting_for_hasher.pop_front().unwrap();
+        while !app.waiting_for_hasher.is_empty() && !app.available_hashers.is_empty() {
+            let hasher = app.available_hashers.pop_front().unwrap();
+            let task = app.waiting_for_hasher.pop_front().unwrap();
             if hasher.send(task).is_err() {
                 warn!("All hashers have crashed");
                 return Ok(());
             }
         }
 
-        while !available_hashers.is_empty()
-            && waiting_for_hasher.is_empty()
-            && !running_disk_scan
-            && completed_pkgs == total_pkgs
+        while !app.available_hashers.is_empty()
+            && app.waiting_for_hasher.is_empty()
+            && !app.running_disk_scan
+            && app.completed_pkgs == app.total_pkgs
         {
-            available_hashers.pop_front();
+            app.available_hashers.pop_front();
+            app.retired_hashers += 1;
         }
 
         if redraw {
-            let mut status = "packages: ".bold().to_string();
-            status.push_str(
-                &format!("{:>7}", completed_pkgs.to_formatted_string(&Locale::en))
-                    .color(if completed_pkgs < total_pkgs {
-                        Color::Yellow
-                    } else {
-                        Color::Green
-                    })
-                    .to_string(),
-            );
-            status.push_str(&"/".bold().to_string());
-            status.push_str(
-                &total_pkgs
-                    .to_formatted_string(&Locale::en)
-                    .bold()
-                    .to_string(),
-            );
-            if running_list_installed {
-                status.push_str("...");
-            }
-
-            if !trusted_hashes.is_empty() {
-                status.push_str(
-                    &format!(
-                        " (files: {:>7})",
-                        trusted_hashes.len().to_formatted_string(&Locale::en)
-                    )
-                    .bright_black()
-                    .to_string(),
-                );
-            }
-
-            status.push_str(&" | scanned disk: ".bold().to_string());
-            status.push_str(
-                &format!(
-                    "{:>8}",
-                    waiting_for_data.len().to_formatted_string(&Locale::en)
-                )
-                .yellow()
-                .to_string(),
-            );
-            if running_disk_scan {
-                status.push_str("...");
-            }
-            status.push('/');
-            status.push_str(&format!(
-                "{:>7}",
-                waiting_for_hasher.len().to_formatted_string(&Locale::en)
-            ));
-
-            status.push_str(&" | hashing ".bold().to_string());
-            {
-                let running_hash_workers = num_hash_worker - available_hashers.len();
-                let s = format!("{running_hash_workers}/{num_hash_worker}");
-                let s = if running_hash_workers == num_hash_worker {
-                    s.cyan()
-                } else if running_hash_workers == 0 {
-                    s.bright_black()
-                } else {
-                    s.normal()
-                }
-                .to_string();
-                write!(status, "[{s}]")?;
-            }
-
-            status.push_str(&" | passed".bold().to_string());
-            status.push('=');
-            status.push_str(
-                &files_passed
-                    .to_formatted_string(&Locale::en)
-                    .green()
-                    .to_string(),
-            );
-            status.push_str(&" failed".bold().to_string());
-            status.push('=');
-            status.push_str(
-                &files_flagged
-                    .to_formatted_string(&Locale::en)
-                    .red()
-                    .to_string(),
-            );
-
-            if let Some(path) = &disk_pwd {
-                let path = format!("{:?}", path.display());
-
-                let mut offset = 0;
-                let mut width = 0;
-                let mut truncated = false;
-                for c in path.chars() {
-                    width += unicode_width::UnicodeWidthChar::width(c).unwrap_or(1);
-                    if width < PATH_TRUNCATE {
-                        offset += c.len_utf8();
-                    } else {
-                        truncated = true;
-                        break;
-                    }
-                }
-
-                let path = &path[..offset];
-                let truncated = if truncated { "..." } else { "" };
-                status.push_str(&format!(" ({path}{truncated})").bright_black().to_string());
-            }
-
-            if args.verbose > 0 {
-                info!("{status}");
-            } else {
-                println!("{status}");
-            }
+            app.redraw(args.verbose > 0);
             redraw = false;
         }
     }
+
+    // redraw one final time
+    app.redraw(args.verbose > 0);
 
     Ok(())
 }
